fb2a50d645baeac24955785820f0550828465465
Thiago Pereira <thiagorpereira7@gmail.com>
2019-11-22 18:10:58 -0200

    Config server, app, routes and install express, nodemon and sucrase

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import routes from './routes';
+
+// import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.server.use(express.json()); // Esta apta a receber requisições tipo json
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
