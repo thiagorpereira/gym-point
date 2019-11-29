835eff650a6d2df0fbef3b4a12edda515c59222f
Thiago Pereira <thiagorpereira7@gmail.com>
2019-11-29 16:49:59 -0200

    Validatind Session data

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,20 +1,33 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import Admin from '../models/Admin';
 
-class SessionController{
+class SessionController {
   async store(req, res) {
-    const { email, password} = req.body;
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
 
-    const admin = await Admin.findOne({ where : { email} });
-
-    if (!admin) {
-      return res.status(401).json({ error: "Admin not found"});
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
     }
 
-    if(!(await admin.checkPassword(password))){
-      return res.status(401).json({ erros: 'Password does not match'});
+    const { email, password } = req.body;
+
+    const admin = await Admin.findOne({ where: { email } });
+
+    if (!admin) {
+      return res.status(401).json({ error: 'Admin not found' });
+    }
+
+    if (!(await admin.checkPassword(password))) {
+      return res.status(401).json({ erros: 'Password does not match' });
     }
 
     const { id, name } = admin;
