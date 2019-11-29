05a02cfae7c2c71e0ef8984e0e3ec3ae24ee5dbb
Thiago Pereira <thiagorpereira7@gmail.com>
2019-11-29 16:47:15 -0200

    Validating admin data

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,20 +1,34 @@
+import * as Yup from 'yup';
 import Admin from '../models/Admin';
 
 class AdminController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
 
-    const adminExists = await Admin.findOne({ where : { email: req.body.email} });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const adminExists = await Admin.findOne({
+      where: { email: req.body.email },
+    });
 
     if (adminExists) {
-      return res.status(400).json({ error: "Admin already exists"});
+      return res.status(400).json({ error: 'Admin already exists' });
     }
 
     const { id, name, email } = await Admin.create(req.body);
 
-    return res.json({ id, name , email});
-
+    return res.json({ id, name, email });
   }
-
 }
 
 export default new AdminController();
