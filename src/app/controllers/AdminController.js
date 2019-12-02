09a3fd866d48b90da6b4fe3ec89df1d98ec604b5
Thiago Pereira <thiagorpereira7@gmail.com>
2019-12-02 13:28:37 -0200

    Build update method to Admin

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -17,15 +17,58 @@ class AdminController {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
+    const { email } = req.body;
+
     const adminExists = await Admin.findOne({
-      where: { email: req.body.email },
+      where: { email },
     });
 
     if (adminExists) {
       return res.status(400).json({ error: 'Admin already exists' });
     }
 
-    const { id, name, email } = await Admin.create(req.body);
+    const { id, name } = await Admin.create(req.body);
+
+    return res.json({ id, name, email });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string()
+        .min(6)
+        .when('password', (password, field) =>
+          password ? field.required().oneOf([Yup.ref('password')]) : field
+        ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { email, oldPassword } = req.body;
+    const admin = await Admin.findByPk(req.adminId);
+
+    if (email !== admin.email) {
+      const adminExists = Admin.findOne({ where: { email } });
+
+      if (adminExists) {
+        return res.status(400).json({ error: 'Admin already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await admin.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name } = await admin.update(req.body);
 
     return res.json({ id, name, email });
   }
